2a2be634c23d2be499686b29d55b74b045f57f67
sourav mishra <souravvmishra@gmail.com>
2025-02-13 14:55:33 +0530

    done

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { signInWithGoogle } from '@/lib/authFunctions';
+import { useRouter } from 'next/navigation';
 
 export default function GoogleLoginButton() {
+    const router = useRouter();
     const handleGoogleLogin = async () => {
         try {
             await signInWithGoogle();
+            router.push('/quadrat')
         } catch (error) {
             console.error('Login failed', error);
         }
